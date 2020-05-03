80891cb8b32924569c0ab9abaea2de84a4af1d1b
achuspran <achuspran03@gmail.com>
2020-05-03 19:48:11 +1000

    initial commit

---
+++ src/components/about.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+        <div>
+            <h4>About Me, Aswathi Prakash.</h4>
+        </div>
+    )
+  }
+}
+
---
+++ src/components/skills.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Skills extends Component {
+  render() {
+    return (
+        <div><h4>My Skills</h4></div>
+    )
+  }
+}
