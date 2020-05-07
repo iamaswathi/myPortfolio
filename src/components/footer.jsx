3447819d5a8e587ef188f60592fc7494c2141f46
Aswathi Prakash <achuspran03@gmail.com>
2020-05-07 23:55:55 +1000

    updated footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -13,10 +13,10 @@ export default class Footer extends Component {
                 </div>
                 <div className="col-sm-6">
                     <ul className="social-icon">
-                        <li><a href="www.google.com"><i className="fa fa-facebook"></i></a></li>
-                        <li><a href="www.google.com"><i className="fa fa-twitter"></i></a></li>
-                        <li><a href="www.google.com"><i className="fa fa-github"></i></a></li>
-                        <li><a href="www.google.com"><i className="fa fa-dribbble"></i></a></li>
+                        <li><a href="www.google.com"><i className="fab fa-facebook"></i></a></li>
+                        <li><a href="www.google.com"><i className="fab fa-twitter"></i></a></li>
+                        <li><a href="www.google.com"><i className="fab fa-github"></i></a></li>
+                        <li><a href="www.google.com"><i className="fab fa-dribbble"></i></a></li>
                     </ul>
                 </div>
             </div>
