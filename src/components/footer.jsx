461ff62997df938768136415f2d2d1ccd374c23f
Aswathi Prakash <achuspran03@gmail.com>
2020-05-07 23:41:21 +1000

    created footer.jsx

---
+++ src/components/footer.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <section className="darkContainer" id="ap-footer">
+        <div className="container">
+          <div className="row sectionSeparator">
+            <div className="col-sm-12 col-md-6">
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
